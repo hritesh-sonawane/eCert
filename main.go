f0bba02fe49ea24e82e476869f57ccfcdb53c912
hritesh93 <hriteshbebop@gmail.com>
2021-07-20 23:50:01 +0530

    add basic shapes

--- main.go
+++ main.go
@@ -26,6 +26,18 @@ func main() {
 	_, lineHt = pdf.GetFontSize()
 	pdf.MultiCell(0, lineHt*1.5, "Here is some text. If it is too long it will be word wrapped automatically. Hunger is the best spice. If there is a new line it will be\nwrapped as well (unlike other ways of writing text in gofpdf).", gofpdf.BorderNone, gofpdf.AlignLeft, false)
 
+	// Basic Shapes
+	pdf.SetFillColor(0, 255, 0)
+	pdf.SetDrawColor(0, 0, 255)
+	pdf.Rect(400, 100, 100, 100, "FD")
+	pdf.SetFillColor(100, 200, 200)
+	pdf.Polygon([]gofpdf.PointType{
+		{110, 250},
+		{160, 300},
+		{110, 350},
+		{60, 300},
+	}, "F")
+
 	err := pdf.OutputFileAndClose("hello.pdf")
 	if err != nil {
 		panic(err)
